ccc45f30f53705939fe4977e4e2d7ff12a4f1ad4
Ion Craciun <6craci69@solent.ac.uk>
2022-10-14 15:43:10 +0100

    Testing PyCharm with GitHub.

--- multiline_message.py
+++ multiline_message.py
@@ -1,3 +1,6 @@
 # Display message to the standard output
 print("System Failure Imminent!")
 print("System reboot has been initiated...")
+print("rebooting sensory system")
+print("reeboting output motors")
+print("rebooting hover engine")
